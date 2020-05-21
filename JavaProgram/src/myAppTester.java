b6bd01d5a3b42e1a81149d8ff01ddd40af92a1a8
Aishwarya Shivanand <shivananda@student.douglascollege.ca>
2020-05-21 01:29:24 -0700

    Modified Java file

--- JavaProgram/src/myAppTester.java
+++ JavaProgram/src/myAppTester.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 
  */
@@ -13,7 +15,28 @@ public class myAppTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+					int[] nw = new int[5];
+			System.out.println("Original array");
+			display(nw);
+			System.out.println("After filling");
+			Arrays.fill(nw, 8);
+			display(nw);
+			System.out.println("After changing value");
+			nw[3]= 5;
+			nw[1]=6;
+			display(nw);
+			System.out.println("After sorting");
+			Arrays.sort(nw,1,4);
+			display(nw);
+			
+		}
+		public static void display(int [] a)
+		{
+			for (int i = 0; i < a.length; i++) {
+				System.out.print(a[i] + " ");
+				
+				
+			}System.out.println();
 	}
 
 }
